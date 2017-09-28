e808967da172e48c586608746dcbd0d76662de43
supritapuned <puned.s@husky.neu.edu>
2017-09-28 15:40:42 -0400

    test

--- src/test/java/com/csye6225/demo/DemoApplicationTests.java
+++ src/test/java/com/csye6225/demo/DemoApplicationTests.java
@@ -6,13 +6,21 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertNull;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-  @Ignore
+ /* @Ignore
   @Test
   public void contextLoads() {
-  }
+  }*/
+
+ @Test
+  public void test()
+ {
+   assertNull(null);
+ }
 
 }
